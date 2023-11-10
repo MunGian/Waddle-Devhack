11ad25ac259c0ff734eb78d9c3f059ecdd24f95d
MunGian <121543760+MunGian@users.noreply.github.com>
2023-11-10 14:12:35 +0800

    Update Group.tsx

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import ViewMoreEventPage1 from "../pages/ViewMoreEventPage1";
+import ViewMoreEventPage from "../pages/ViewMoreEventPage";
 import styles from "./Group.module.css";
 
 const Group: FunctionComponent = () => {
@@ -11,7 +11,7 @@ const Group: FunctionComponent = () => {
       </div>
       <div className={styles.sun19Nov}>{`Sun, 19 Nov `}</div>
       <img className={styles.image38Icon} alt="" src="/image-382@2x.png" />
-      <ViewMoreEventPage1 />
+      <ViewMoreEventPage />
     </div>
   );
 };
